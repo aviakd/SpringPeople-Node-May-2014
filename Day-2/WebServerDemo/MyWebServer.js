4c29bfd6d407ab0d5fcd7744d33fe57976e91cdf
tkmagesh <tkmagesh77@gmail.com>
2014-05-10 11:17:03 +0530

    Web Server serving static resources

--- Day-2/WebServerDemo/MyWebServer.js
+++ Day-2/WebServerDemo/MyWebServer.js
@@ -1,12 +1,32 @@
-var http = require("http");
+var http = require("http"),
+	url = require("url"),
+	path = require("path"),
+	fs = require("fs");
+
+
 var connectionHandler = function(req,res){
 	console.log("A new connection is established for " + req.url);
+
 	if (req.url === "/favicon.ico"){
 		res.statusCode = 404;
+		res.end();
 	} else {
-		res.write("<h1>Welcome to Node</h1>");
+		var resourcePath = path.join(__dirname, url.parse(req.url).pathname);
+		if (fs.existsSync(resourcePath)){
+			var readStream = fs.createReadStream(resourcePath,{encoding : "utf8"});
+			readStream.on("data",function(data){
+				res.write(data);
+			});
+			readStream.on("end",function(){
+				res.end();
+			});
+		} else {
+			console.log(resourcePath + "-- not found");
+			res.statusCode = 404;
+			res.end();
+		}
 	}
-	res.end();
+	
 }
 var server = http.createServer(connectionHandler);
 server.listen(9090);
